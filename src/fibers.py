43bc2c3206ad027cf4d45300592698663387ad32
Travis Nesland <nesland@musc.edu>
2012-03-12 19:39:47 -0400

    Add ability to read trackvis files.

--- src/fibers.py
+++ src/fibers.py
@@ -2,9 +2,89 @@
 class TNFibers(object):
     """Container class for fibers"""
 
-    def __init__(self):
-        pass
+    def __init__(self,
+                 data = None,
+                 format = 'trackvis',
+                 header = None,
+                 spacing = 'mm'
+    ):
+        self.data = data
+        self.format = format
+        self.header = header
+        self._load_alternative_spacings()
+        
+    def get_data(self):
+        return self.data
 
+    def get_dim(self):
+        if self.format == 'trackvis':
+            if self.header:
+                return self.get_value_for_header_key('dim')
+        
+    def get_header(self):
+        return self.header
+        
+    def get_data(self):
+        if self.format == 'trackvis':
+            return self.data
+
+    def get_voxel_size(self):
+        if self.format == 'trackvis':
+            if self.header is not None:
+                return self.get_value_for_header_key('voxel_size')
+
+    def get_value_for_header_key(self, key):
+        if self.format == 'trackvis':
+            try:
+                return self.header[self._trackvis_header_fields()[key]]
+            except:
+                return None
+
+    def _load_alternative_spacings(self):
+        """Populate the data with both voxel and mm based spacing
+        values.
+
+        The outcome is that within data, there will be keys for
+        both.
+
+        """
+        voxel_size = self.get_voxel_size()
+        if voxel_size is not None:
+            if not self.data[0].has_key('vertices_vox'):
+                for idx in self.data:
+                    self.data[idx]['vertices_vox'] = self.data[idx]['vertices_mm'] / voxel_size
+            if not self.data[0].has_key('vertices_mm'):
+                for idx in self.data:
+                    self.data[idx]['vertices_mm'] = self.data[idx]['vertices_vox'] * voxel_size
+        
+    def _trackvis_header_fields(self):
+        return {
+            'id_string': 0,
+            'dim': 1,
+            'voxel_size': 2,
+            'origin': 3,
+            'n_scalars': 4,
+            'scalar_name': 5,
+            'n_properties': 6,
+            'property_name': 7,
+            'vox_to_ras': 8,
+            'reserved': 9,
+            'voxel_order': 10,
+            'pad2': 11,
+            'image_orientation_patient': 12,
+            'pad1': 13,
+            'invert_x': 14,
+            'invert_y': 15,
+            'invert_z': 16,
+            'swap_xy': 17,
+            'sway_yz': 18,
+            'swap_zx': 19,
+            'n_count': 20,
+            'version': 21,
+            'hdr_size': 22
+        }
+                
+        
 # Module Level Functions
 def sum_of_inverse_fiber_lengths(roi_img, endpoints, lengths):
     """For a given ROI atlas and set of fibers, return an adjacency
@@ -48,3 +128,19 @@ def sum_of_inverse_fiber_lengths(roi_img, endpoints, lengths):
             print e
             raise
     return result
+
+def read(file, format='trackvis'):
+    """Load a TNFibers object from file and return"""
+    accepted_formats = ['trackvis']
+    if format not in accepted_formats:
+        print "%s is not an accepted format" % format
+        raise
+    if format == 'trackvis':
+        import nibabel.trackvis
+        import numpy
+        read_data = nibabel.trackvis.read(file)
+        my_data = dict()
+        for idx, rec in enumerate(read_data[0]):
+            my_data[idx] = {'vertices_mm': rec[0]}
+        my_header = read_data[1].tolist()
+        return TNFibers(data=my_data, format='trackvis', header=my_header)
--- src/test_fibers.py
+++ src/test_fibers.py
@@ -1,7 +1,13 @@
 import unittest
 import fibers
 
-SUPPORT_DIR = 'test_data/fibers'
+FIBERS_DIR = 'test_data/fibers'
+IMAGES_DIR = 'test_data/images/3dim_images'
+
+
+f = fibers.read("%s/dti.trk" % FIBERS_DIR) # load once to avoid excess
+                                           # run time
+
 
 class Test_TNImages(unittest.TestCase):
 
@@ -12,5 +18,37 @@ class Test_TNImages(unittest.TestCase):
         pass
 
     def test_can_get_sum_of_inverse_lengths(self):
+        # TODO: I need a better test for this, basically I need to
+        # generate well-defined NIFTI images w/ more complexity than I
+        # currently have and test the results
         pass
         
+    def test_can_load_trackvis(self):
+        global f
+        assert f is not None
+
+    def test_can_get_data(self):
+        # test trackfile has a fiber count of 157434 according to
+        # trackvis
+        global f
+        assert len(f.get_data()) == 157434
+
+    def test_can_get_voxel_size(self):
+        # test trackfile has a voxel size of [2,2,2]
+        global f
+        assert (f.get_voxel_size() == [2,2,2]).all()
+
+    def test_can_get_dim(self):
+        # test trackfile has dims of [104, 104, 60]
+        global f
+        assert (f.get_dim() == [104, 104, 60]).all()
+
+    def test_can_get_voxel_spacing(self):
+        # test trackfile has a voxel size of [2,2,2], so a given
+        # vertex in voxel coordinates should be equal to half of the
+        # same vertex in mm coordinates
+        global f
+        streamlines = f.get_data()
+        mm_vertex = streamlines[0]['vertices_mm'][0]
+        vox_vertex = streamlines[0]['vertices_vox'][0]
+        assert ((mm_vertex / vox_vertex) == 2).all()
