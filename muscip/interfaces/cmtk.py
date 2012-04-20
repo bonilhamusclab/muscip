3ecd79d1308a926fa8671e5dded1680ba350fb4b
Travis Nesland <nesland@musc.edu>
2012-04-20 16:51:54 -0400

    Continue to work on atlas extraction.

--- muscip/interfaces/cmtk.py
+++ muscip/interfaces/cmtk.py
@@ -16,7 +16,6 @@ except Exception, e:
     have_cmp = False
     warnings.warn('cmp not installed')
 else:
-    import cmp
     from cmp.util import runCmd
 
 def generate_WM_and_GM_mask(subjects_dir, subject_id, wm_mask_filename, roi_filename):
@@ -32,15 +31,12 @@ def generate_WM_and_GM_mask(subjects_dir, subject_id, wm_mask_filename, roi_file
     
     fs_dir = op.join(subjects_dir,subject_id)
     output_dir = op.abspath(op.curdir)
-    cmp_config = cmp.configuration.PipelineConfiguration()
-    cmp_config.parcellation_scheme = "NativeFreesurfer"
-    log = cmp_config.get_logger()
     
     # need to convert
     mri_cmd = 'mri_convert -i "%s/mri/aparc+aseg.mgz" -o "%s/mri/aparc+aseg.nii.gz"' % (fs_dir, fs_dir)
-    runCmd( mri_cmd, log )
+    runCmd( mri_cmd, None )
 
-    fout = op.join(fs_dir, 'mri', 'aparc+aseg.nii.gz')    
+    fout = op.join(fs_dir, 'mri', 'aparc+aseg.nii.gz') ##OUTPUT
     niiAPARCimg = ni.load(fout)
     niiAPARCdata = niiAPARCimg.get_data()
 
@@ -61,23 +57,25 @@ def generate_WM_and_GM_mask(subjects_dir, subject_id, wm_mask_filename, roi_file
 #    OTHER = {1:[16],
 #             2:[83]}
 
-    MAPPING = [[1,2012],[2,2019],[3,2032],[4,2014],[5,2020],[6,2018],[7,2027],[8,2028],[9,2003],[10,2024],[11,2017],[12,2026],
-               [13,2002],[14,2023],[15,2010],[16,2022],[17,2031],[18,2029],[19,2008],[20,2025],[21,2005],[22,2021],[23,2011],
-               [24,2013],[25,2007],[26,2016],[27,2006],[28,2033],[29,2009],[30,2015],[31,2001],[32,2030],[33,2034],[34,2035],
-               [35,49],[36,50],[37,51],[38,52],[39,58],[40,53],[41,54],[42,1012],[43,1019],[44,1032],[45,1014],[46,1020],[47,1018],
-               [48,1027],[49,1028],[50,1003],[51,1024],[52,1017],[53,1026],[54,1002],[55,1023],[56,1010],[57,1022],[58,1031],
-               [59,1029],[60,1008],[61,1025],[62,1005],[63,1021],[64,1011],[65,1013],[66,1007],[67,1016],[68,1006],[69,1033],
-               [70,1009],[71,1015],[72,1001],[73,1030],[74,1034],[75,1035],[76,10],[77,11],[78,12],[79,13],[80,26],[81,17],
-               [82,18],[83,16]]
+    MAPPING = [[1,2012],[2,2019],[3,2032],[4,2014],[5,2020],[6,2018],[7,2027],[8,2028],[9,2003],[10,2024],
+               [11,2017],[12,2026],[13,2002],[14,2023],[15,2010],[16,2022],[17,2031],[18,2029],[19,2008],
+               [20,2025],[21,2005],[22,2021],[23,2011],[24,2013],[25,2007],[26,2016],[27,2006],[28,2033],
+               [29,2009],[30,2015],[31,2001],[32,2030],[33,2034],[34,2035],[35,49],[36,50],[37,51],[38,52],
+               [39,58],[40,53],[41,54],[42,1012],[43,1019],[44,1032],[45,1014],[46,1020],[47,1018],[48,1027],
+               [49,1028],[50,1003],[51,1024],[52,1017],[53,1026],[54,1002],[55,1023],[56,1010],[57,1022],
+               [58,1031],[59,1029],[60,1008],[61,1025],[62,1005],[63,1021],[64,1011],[65,1013],[66,1007],
+               [67,1016],[68,1006],[69,1033],[70,1009],[71,1015],[72,1001],[73,1030],[74,1034],[75,1035],
+               [76,10],[77,11],[78,12],[79,13],[80,26],[81,17],[82,18],[83,16]]
 
-    WM = [2, 29, 32, 41, 61, 64, 59, 60, 27, 28] +  range(77,86+1) + range(100, 117+1) + range(155,158+1) + range(195,196+1) + range(199,200+1) + range(203,204+1) + [212, 219, 223] + range(250,255+1)
+    WM = [2, 29, 32, 41, 61, 64, 59, 60, 27, 28] + range(77,86+1) + range(100, 117+1) + \
+         range(155,158+1) + range(195,196+1) + range(199,200+1) + range(203,204+1) + \
+         [212, 219, 223] + range(250,255+1)
     # add
     # 59  Right-Substancia-Nigra
     # 60  Right-VentralDC
     # 27  Left-Substancia-Nigra
     # 28  Left-VentralDC
 
-    log.info("WM mask....")
     #%% create WM mask    
     niiWM = np.zeros( niiAPARCdata.shape, dtype = np.uint8 )
 
@@ -89,54 +87,47 @@ def generate_WM_and_GM_mask(subjects_dir, subject_id, wm_mask_filename, roi_file
 #         niiWM[niiAPARCdata == i] = 1
 
     img = ni.Nifti1Image(niiWM, niiAPARCimg.get_affine(), niiAPARCimg.get_header())
-    log.info("Save to: " + WMout)
     ni.save(img, WMout)
 
-    log.info("GM mask....")    
     #%% create GM mask (CORTICAL+SUBCORTICAL)
     #%  -------------------------------------
-    for park in cmp_config._get_lausanne_parcellation().keys():
-        log.info("Parcellation: " + park)
-        GMout = op.join(output_dir, roi_filename)
-
-        niiGM = np.zeros( niiAPARCdata.shape, dtype = np.uint8 )
-
-        for ma in MAPPING:
-            niiGM[ niiAPARCdata == ma[1]] = ma[0]
-
+    GMout = op.join(output_dir, roi_filename)
+    niiGM = np.zeros( niiAPARCdata.shape, dtype = np.uint8 )
+    for ma in MAPPING:
+        niiGM[ niiAPARCdata == ma[1]] = ma[0]
 #        # % 33 cortical regions (stored in the order of "parcel33")
 #        for idx,i in enumerate(CORTICAL[1]):
 #            niiGM[ niiAPARCdata == (2000+i)] = CORTICAL[2][idx] # RIGHT
 #            niiGM[ niiAPARCdata == (1000+i)] = CORTICAL[2][idx] + 41 # LEFT
-#
 #        #% subcortical nuclei
 #        for idx,i in enumerate(SUBCORTICAL[1]):
 #            niiGM[ niiAPARCdata == i ] = SUBCORTICAL[2][idx]
-#
 #        # % other region to account for in the GM
 #        for idx, i in enumerate(OTHER[1]):
 #            niiGM[ niiAPARCdata == i ] = OTHER[2][idx]
-
-        log.info("Save to: " + GMout)        
         img = ni.Nifti1Image(niiGM, niiAPARCimg.get_affine(), niiAPARCimg.get_header())
         ni.save(img, GMout)
 
-    log.info("[DONE]")
+def crop_and_move_WM_and_GM(subjects_dir, subject_id, wm_mask_filename, roi_filename):
+    """Taken from cmtk and adapted for running as isolated function in
+    this interface.
 
-def crop_and_move_WM_and_GM():
+    http://github.com/tnez/cmp/blob/master/cmp/stages/parcellation/maskcreation.py
 
-    fs_dir = gconf.get_fs()
-    reg_path = gconf.get_cmp_tracto_mask()
+    """
+    import os.path as op
+    import nibabel as ni
+    import numpy as np
 
-    log.info("Cropping and moving datasets to %s" % reg_path)
+    fs_dir = op.join(subjects_dir,subject_id)
+    output_dir = op.abspath(op.curdir)
 
     # datasets to crop and move: (from, to)
     ds = [
-          (op.join(fs_dir, 'mri', 'fsmask_1mm.nii.gz'), op.join(reg_path, 'fsmask_1mm.nii.gz') ),
+          (op.join(fs_dir, 'mri', 'fsmask_1mm.nii.gz'), op.join(output_dir, 'fsmask_1mm.nii.gz') ),
           ]
 
-    for p in gconf.parcellation.keys():
-        ds.append( (op.join(fs_dir, 'mri', 'ROIv_%s.nii.gz' % p), op.join(reg_path, p, 'ROIv_HR_th.nii.gz')) )
+    ds.append( (op.join(fs_dir, 'mri', 'ROIv_%s.nii.gz' % p), op.join(ouput_dir, 'ROIv_HR_th_%s.nii.gz')) )
 
     orig = op.join(fs_dir, 'mri', 'orig', '001.mgz')
 
